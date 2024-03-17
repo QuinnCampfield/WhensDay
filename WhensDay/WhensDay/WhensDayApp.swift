7d2698626d326d513e95d662a495a5707c1266a2
Quinn C <quinnc@quinns-laptop.lan>
2024-03-16 22:24:40 -0700

    Start

---
+++ WhensDay/WhensDay/WhensDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhensDayApp.swift
+//  WhensDay
+//
+//  Created by Quinn C on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct WhensDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
